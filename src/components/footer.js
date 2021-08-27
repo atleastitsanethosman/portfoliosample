abdaa1750fa07c156453a06d2033d85e296b7c77
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-08-27 17:54:38 -0400

    updating footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const styles = {
   heroStyle: {
@@ -9,11 +10,15 @@ const styles = {
 export default function Footer() {
   
   return (
-    <nav className="footer-nav">
+    
       <header className="header">
-      <h4>2021 | Ian Hennessey</h4>
+        <nav className="footer-nav">
+        <ul>
+          <a target="_blank" href="https://www.linkedin.com/in/ian-hennessey-b4310a38"> <li><span><FontAwesomeIcon icon={['fab', 'linkedin']} /></span> Linkedin</li> </a>
+          <a target ="_blank" href="https://github.com/atleastitsanethosman"><li><span><FontAwesomeIcon icon={['fab', 'github']} /></span> GitHub</li></a>
+          <a href="mailto:ian.hennessey@gmail.com"> <li><span><FontAwesomeIcon icon={['fas', 'envelope-square']} /></span> Email</li></a>
+        </ul>
+        </nav>
       </header>
-      
-    </nav>
   );
 }
