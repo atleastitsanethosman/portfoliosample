cd47998d9e8b359f34d498c404931757bd74a2f5
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-08-30 16:53:45 -0400

    adding email functionality to contact form

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-const styles = {}
-
 export default function Footer() {
   
   return (
@@ -12,7 +10,6 @@ export default function Footer() {
         <ul>
           <a target="_blank" href="https://www.linkedin.com/in/ian-hennessey-b4310a38"> <li><span><FontAwesomeIcon icon={['fab', 'linkedin']} /></span> Linkedin</li> </a>
           <a target ="_blank" href="https://github.com/atleastitsanethosman"><li><span><FontAwesomeIcon icon={['fab', 'github']} /></span> GitHub</li></a>
-          <a href="mailto:ian.hennessey@gmail.com"> <li><span><FontAwesomeIcon icon={['fas', 'envelope-square']} /></span> Email</li></a>
         </ul>
         </nav>
       </footer>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {useForm} from 'react-hook-form'
+import{ init, sendForm } from 'emailjs-com';
+init("user_5o4fYZ0JYcNmlg4LMm17k");
 
 export default function Contact() {
+    
+  const { register, handleSubmit, formState: { errors } } = useForm({mode: "onBlur"});
+  const onSubmit = (data) => { 
+    sendForm('portfoliocontact', 'portfolio_contact', '#contact-form')
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+      console.log('FAILED...', error);
+    });
+    
+  };
+  
     return(
     <section className="contact">
       <div>
       <h3>Contact</h3>
       </div>
-      <section className="contact-form">
+      <section className="contact-form" onSubmit={handleSubmit(onSubmit)}>
         <form id='contact-form'>
-          <input type='text' name='user_name' placeholder='Name' />
+          {errors.name && <div className="warning">Name Required!</div>}
+          <input type='text' {...register("name", {required: true, maxLength: 50})} name='name' placeholder='Name' />
           <br/>
-          <input type='email' name='user_email' placeholder='Email' />
+          {errors.email && <div className="warning">Valid Email Required!</div>}
+          <input type='email' {...register("email", {required: true, pattern: /^\S+@\S+\.\S+$/})} name='email' placeholder='Email' />
           <br/>
-          <textarea name='message' placeholder='Message'/>
+          {errors.message && <div className="warning">Message between 10 and 1500 Characters Required!</div>}
+          <textarea {...register("message", {required: true, min:10, max: 1500})} name='message' placeholder='Message'/>
           <br/>
           <input type='submit' value='Send' />
         </form>
