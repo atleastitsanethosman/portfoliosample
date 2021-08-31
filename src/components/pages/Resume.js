d68b1ec827cde0a94f915d8c6134aa509ec4c7a2
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-08-31 15:49:35 -0400

    failed to save manifest file, pushing again

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,17 +8,17 @@ export default function Resume() {
                 <h3>Resume</h3>
             </div>
             <div className="resumeRender">
-            <ul className="skillsList">
-                <li><h4>Skillset:</h4></li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>MySQL</li>
-                <li>MongoDB</li>
-                <li>BootStrap</li>
-                <li>Express</li>
-                <li>NodeJS</li>
-            </ul>
+                <ul className="skillsList">
+                    <li><h4>Skillset:</h4></li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>BootStrap</li>
+                    <li>Express</li>
+                    <li>NodeJS</li>
+                </ul>
                 <a href={ process.env.PUBLIC_URL + "/images/20210726wd-Resume_Ian.Hennessey(MI).pdf"} download> <span><FontAwesomeIcon icon={['fas', 'file']}/></span> Download Resume </a>
             </div>
 
